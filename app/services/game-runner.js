b1746c84dac99e340eb59ce2cb342dd92bae8e4e
Manos Fountoulakis <fountoulakismanos@gmail.com>
2018-01-18 16:48:14 -0500

    define startup function

--- app/services/game-runner.js
+++ app/services/game-runner.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
+import Kitty from '../kitty';
+import Sky from '../sky';
+import Cloud from '../cloud';
+import Ground from '../ground';
+import Mount from '../mount';
+import FrameRateCounter from '../frameRateCounter';
 
 export default Ember.Service.extend({
+    playing: false,
+
+    startUp(theCanvas, context) {
+
+        this.theCanvas = theCanvas
+        this.context = context
+
+        //INSTANTIATE ALL OBJECTS
+        this.load(theCanvas, context);
+        //START THE MENU SCREEN GAMELOOP
+        this.menuGameLoop(theCanvas, context);
+    }
 });
