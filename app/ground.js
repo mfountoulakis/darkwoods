3303154425d8a452bff6e15205d1bb2bf322c40c
Manos Fountoulakis <fountoulakismanos@gmail.com>
2018-01-25 01:33:35 -0500

    animate the ground

--- app/ground.js
+++ app/ground.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 
 export default class {
 
-    constructor() {
+    constructor(theCanvas) {
+        this.play = false;
+
+        this.ground_dist = 1;
+        this.groundx = 0;
 
     }
 
@@ -15,7 +19,26 @@ export default class {
     draw(theCanvas, context) {
         context.drawImage(this.buildImage(theCanvas, context, 'assets/images/ground_1.png'), 0, 0);
         context.drawImage(this.buildImage(theCanvas, context, 'assets/images/ground_2.png'), 0, 0);
-        context.drawImage(this.buildImage(theCanvas, context, 'assets/images/ground_3.png'), 0, 0);
+
+        //animate this ground
+        context.drawImage(this.buildImage(theCanvas, context, 'assets/images/ground_3.png'), this.groundx, 0, theCanvas.width, theCanvas.height, 0, 0, theCanvas.width, 1090);
 
     }
+
+    playState(theCanvas, context) {
+       this.play = true;
+    }
+    
+    getGroundx(){
+        return this.groundx
+    }
+
+    update(theCanvas, context) {
+        if ((this.groundx + this.ground_dist) < theCanvas.width && this.play == true)
+            this.groundx += this.ground_dist;
+        else
+            this.groundx = 0
+    }
 }
+
+
