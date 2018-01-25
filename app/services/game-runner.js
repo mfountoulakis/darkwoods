eb590b35ae590fc249983e51f15d6ced571d8a71
Manos Fountoulakis <fountoulakismanos@gmail.com>
2018-01-25 10:51:26 -0500

    instantiate new objects

--- app/services/game-runner.js
+++ app/services/game-runner.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-// import Kitty from '../kitty';
+import Kitty from '../kitty';
 import Sky from '../sky';
 import Cloud from '../cloud';
 import Ground from '../ground';
@@ -9,8 +9,8 @@ export default Ember.Service.extend({
     playing: false,
 
     startUp(theCanvas, context) {
-        this.theCanvas = theCanvas
-        this.context = context
+        this.theCanvas = theCanvas;
+        this.context = context;
         //INSTANTIATE ALL OBJECTS
         this.load(theCanvas, context);
         //START THE MENU SCREEN GAMELOOP
@@ -24,11 +24,17 @@ export default Ember.Service.extend({
         window.requestAnimationFrame(this.menuGameLoop.bind(this));
     },
 
+    AddGameControls() {
+        window.addEventListener('keydown', this.controlAction.bind(this));
+    },
+
     load(theCanvas, context) {
-        this.sky = new Sky(this.theCanvas, this.context)
-        this.mount = new Mount(this.theCanvas, this.context)
-        this.ground = new Ground(this.theCanvas, this.context)
-        this.clouds = new Cloud(this.theCanvas, this.context)
+        this.sky = new Sky(this.theCanvas, this.context);
+        this.mount = new Mount(this.theCanvas, this.context);
+        this.ground = new Ground(this.theCanvas, this.context);
+        this.clouds = new Cloud(this.theCanvas, this.context);
+        this.kitty = new Kitty(this.theCanvas, this.context);
+
     },
 
     render(theCanvas, context) {
@@ -37,12 +43,32 @@ export default Ember.Service.extend({
         this.mount.draw(this.theCanvas, this.context);
         this.ground.draw(this.theCanvas, this.context);
         this.clouds.draw(this.theCanvas, this.context);
+        this.kitty.draw(this.theCanvas, this.context, this.ground.getGroundx(this.theCanvas, this.context));
+    },
+
+    play() {
+        this.playing == true
+        //add ingame controls
+        this.AddGameControls()
+        this.kitty.playState()
+        this.ground.playState()
 
     },
 
     update(theCanvas, context) {
+
         this.clouds.update(this.theCanvas, this.context);
+        this.ground.update(this.theCanvas, this.context);
+        this.kitty.update(this.theCanvas, this.context);
+
     },
+
+    //GAME CONTROLS
+    controlAction(e) {
+        if (e.keyCode === 38) {
+            this.kitty.jump();
+        }
+    }
 
 });
 
