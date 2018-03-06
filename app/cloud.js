8faca116175dbada1d5170e5412a0e3103fb7140
Manos Fountoulakis <fountoulakismanos@gmail.com>
2018-03-05 23:49:12 -0500

    extract map algorithm to function

--- app/cloud.js
+++ app/cloud.js
@@ -1,30 +1,38 @@
 import Ember from 'ember';
 
 export default class {
-    constructor() {
-        var imageSource = 'assets/images/clouds_1.png';
-        this.imageSource = imageSource
+  constructor() {
+    var imageSource = 'assets/images/clouds_1.png';
+    this.imageSource = imageSource;
 
-        this.cloud_dist = 0.5;  
-        this.cloudx = 2;  
-    }
+    this.cloud_dist = 0.5;
+    this.cloudx = 2;
+  }
 
-    buildImage(theCanvas, context) {
-        var image = new Image()
-        image.src = this.imageSource
+  buildImage(theCanvas, context) {
+    var image = new Image();
+    image.src = this.imageSource;
 
-        return image
-    }
+    return image;
+  }
 
-    draw(theCanvas, context) {
-        context.drawImage(this.buildImage(theCanvas, context), this.cloudx, 0, theCanvas.width, theCanvas.height, 0, 0, theCanvas.width, 900);
-    }
+  draw(theCanvas, context) {
+    // context.setTransform(1, 0, 0, 1, 0, 0);
+    // context.drawImage(
+    //   this.buildImage(theCanvas, context),
+    //   this.cloudx,
+    //   0,
+    //   theCanvas.width,
+    //   theCanvas.height,
+    //   0,
+    //   0,
+    //   theCanvas.width,
+    //   900
+    // );
+  }
 
-    update(theCanvas, context) {
-        if ((this.cloudx + this.cloud_dist) > this.cloud_dist)
-            this.cloudx -= this.cloud_dist;
-        else
-            this.cloudx = theCanvas.width
-    }
-
+  update(theCanvas, context) {
+    if (this.cloudx + this.cloud_dist > this.cloud_dist) this.cloudx -= this.cloud_dist;
+    else this.cloudx = theCanvas.width;
+  }
 }
--- app/services/game-runner.js
+++ app/services/game-runner.js
@@ -6,90 +6,93 @@ import Ground from '../ground';
 import Mount from '../mount';
 
 export default Ember.Service.extend({
-    playing: false,
+  playing: false,
 
-    startUp(theCanvas, context) {
-        this.theCanvas = theCanvas;
-        this.context = context;
-        //INSTANTIATE ALL OBJECTS
-        this.load(theCanvas, context);
-        //START THE MENU SCREEN GAMELOOP
-        this.menuGameLoop(theCanvas, context);
-    },
+  startUp(theCanvas, context) {
+    this.AddGameControls();
 
-    menuGameLoop(theCanvas, context) {
-        this.render(theCanvas, context);
-        this.update(theCanvas, context);
-        //check if menu should be rendered
-        window.requestAnimationFrame(this.menuGameLoop.bind(this));
-    },
+    this.theCanvas = theCanvas;
+    this.context = context;
+    //INSTANTIATE ALL OBJECTS
+    this.theCanvas.width = 1067;
+    this.theCanvas.height = 600;
 
-    AddGameControls() {
-        window.addEventListener('keydown', this.controlAction.bind(this));
-        window.addEventListener('keyup', this.controlAction.bind(this));
+    this.load(theCanvas, context);
 
-    },
+    console.log(this.bg.mapRows);
+    console.log(this.bg.mapCols);
 
-    load(theCanvas, context) {
-        this.sky = new Sky(this.theCanvas, this.context);
-        this.mount = new Mount(this.theCanvas, this.context);
-        this.ground = new Ground(this.theCanvas, this.context);
-        this.clouds = new Cloud(this.theCanvas, this.context);
-        this.kitty = new Kitty(this.theCanvas, this.context);
+    this.menuGameLoop(theCanvas, context);
+    // this.gameLoop(theCanvas, context);
+  },
 
-    },
+  menuGameLoop(theCanvas, context) {
+    this.render(theCanvas, context);
+    this.update(theCanvas, context);
+    this.play();
 
-    render(theCanvas, context) {
-        this.context.clearRect(0, 0, theCanvas.width, theCanvas.height);
-        this.sky.draw(this.theCanvas, this.context);
-        this.mount.draw(this.theCanvas, this.context);
-        this.ground.draw(this.theCanvas, this.context);
-        this.clouds.draw(this.theCanvas, this.context);
-        this.kitty.draw(this.theCanvas, this.context, this.ground.getGroundx(this.theCanvas, this.context));
-    },
+    //check if menu should be rendered
+    window.requestAnimationFrame(this.menuGameLoop.bind(this));
+  },
 
-    play() {
-        this.playing == true
-        //add ingame controls
-        this.AddGameControls()
-        this.kitty.playState()
-        this.ground.playState()
+  AddGameControls() {
+    window.addEventListener('keydown', this.controlAction.bind(this));
+    window.addEventListener('keyup', this.controlAction.bind(this));
+  },
 
-    },
+  load(theCanvas, context) {
+    this.bg = new Bg(this.theCanvas, this.context);
+    this.camera = new Camera(theCanvas, context);
 
-    update(theCanvas, context) {
-        this.clouds.update(this.theCanvas, this.context);
-        this.ground.update(this.theCanvas, this.context);
-        this.kitty.update(this.theCanvas, this.context);
+    this.sky = new Sky(this.theCanvas, this.context);
+    this.mount = new Mount(this.theCanvas, this.context);
+    this.ground = new Ground(this.theCanvas, this.context);
+    this.clouds = new Cloud(this.theCanvas, this.context);
+    this.kitty = new Kitty(this.theCanvas, this.context);
+  },
 
-    },
+  render(theCanvas, context) {
+    this.context.clearRect(0, 0, theCanvas.width, theCanvas.height);
+    this.bg.draw(this.theCanvas, this.context);
+    // this.camera.draw(this.theCanvas, this.context);
+    this.sky.draw(this.theCanvas, this.context);
+    this.mount.draw(this.theCanvas, this.context);
+    this.ground.draw(this.theCanvas, this.context, this.kitty.getKittyX(), this.kitty.Moving());
+    this.clouds.draw(this.theCanvas, this.context);
+    this.kitty.draw(this.theCanvas, this.context);
+  },
 
+  play() {},
 
-    //GAME CONTROLS
-    controlAction(e) {
-        const type = e.type
-        switch (type) {
-            case "keydown":
-                if (e.keyCode === 38) {
-                    this.kitty.jump();
-                }
-                if (e.keyCode === 39) {
-                    console.log(this.context)
-                    this.kitty.move(this.theCanvas, this.context);
-                }
-                break;
-            case "keyup":
-                if (e.keyCode === 39) {
-                    console.log(this.context)
-                    this.kitty.stop(this.theCanvas, this.context);
-                }
-                break;
-            default:
+  update(theCanvas, context) {
+    this.clouds.update(this.theCanvas, this.context);
+    this.ground.update(this.theCanvas, this.context);
+    this.kitty.update(this.theCanvas, this.context);
+    this.bg.update(this.theCanvas, this.context);
+  },
 
+  //GAME CONTROLS
+  controlAction(e) {
+    const type = e.type;
+    //make sure no scrolling happens before key is pressed.
+    this.bg.camera.dx = 0;
+    this.bg.camera.dy = 0;
+
+    switch (type) {
+      case 'keydown':
+        if (e.keyCode === 38) {
+          this.kitty.jump();
         }
-
+        if (e.keyCode === 39) {
+          this.bg.camera.dx = this.bg.camera.scrollRate;
+        }
+        break;
+      case 'keyup':
+        if (e.keyCode === 39) {
+          // this.kitty.stop(this.theCanvas, this.context);
+        }
+        break;
+      default:
     }
+  },
 });
-
-
-
