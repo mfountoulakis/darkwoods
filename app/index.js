5e1fbd3e63efa7d4e8cc6d87a0020f18eab91fae
Manos Fountoulakis <fountoulakismanos@gmail.com>
2018-03-16 14:28:37 -0400

    draws a line on the canvas

--- app/index.js
+++ app/index.js
@@ -4,24 +4,21 @@ var choo = require('choo');
 // import choo's template helper
 var html = require('choo/html');
 
+var main = require('./templates/main.js');
+
 // initialize choo
 var app = choo();
 
 // create a route
 app.route('/', main);
 
+// start app
 app.mount('body');
 
-function main(state, emit) {
-  return html`
-    <body>
-      <div>ANIMALS</div>
-    </body>
-  `;
-}
-
-// start app
-window.onload = function() {
-  console.log('loaded');
-  app.mount('div');
-};
+// function main(state, emit) {
+//   return html`
+//     <body>
+//       <div>ANIMALS</div>
+//     </body>
+//   `;
+// }
--- app/templates/main.js
+++ app/templates/main.js
@@ -1,1 +1,21 @@
-<div />;
+var html = require('choo/html');
+// const onload = require('on-load');
+
+// export module
+module.exports = function() {
+  // create html template
+  function initCanvas() {
+    var c = document.getElementById('myCanvas');
+    var ctx = c.getContext('2d');
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+  }
+  return html`
+  <body onload=${() => initCanvas()} >
+    <canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000;"> 
+  </body>
+  `;
+};
+
+// `<div onload=${() => console.log('loaded!')}></div>`
